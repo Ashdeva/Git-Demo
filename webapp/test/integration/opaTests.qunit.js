c4f173c656da6160d9413fc3632e272eb03a8ff1
asha p <ashachowdhary203@gmail.com>
2024-03-21 08:21:19 +0000

    Initial commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["ns/gitdemoapp/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ns/git_demoapp/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
